fbf84251756e90f734aa0545042b4c4f978e9756
Simon Dimitriadis <pumba6@gmx.at>
2017-11-22 16:05:52 +0100

    red test

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -24,42 +24,42 @@ public class TennisTest {
     @Parameters
     public static Collection<Object[]> getAllScores() {
         return Arrays.asList(new Object[][] {
-                { 0, 0, "Love-All" },
-                { 1, 1, "Fifteen-All" },
-                { 2, 2, "Thirty-All"},
-                { 3, 3, "Deuce"},
-                { 4, 4, "Deuce"},
-                { 5, 5, "Deuce"},
-                { 6, 6, "Deuce"},
-                { 7, 7, "Deuce"},
-                { 1, 0, "Fifteen-Love"},
-                { 0, 1, "Love-Fifteen"},
-                { 2, 0, "Thirty-Love"},
-                { 0, 2, "Love-Thirty"},
-                { 3, 0, "Forty-Love"},
-                { 0, 3, "Love-Forty"},
-                { 4, 0, "Win for player1"},
-                { 0, 4, "Win for player2"},
-                { 2, 1, "Thirty-Fifteen"},
-                { 1, 2, "Fifteen-Thirty"},
-                { 3, 1, "Forty-Fifteen"},
-                { 1, 3, "Fifteen-Forty"},
-                { 4, 1, "Win for player1"},
-                { 1, 4, "Win for player2"},
-                { 3, 2, "Forty-Thirty"},
-                { 2, 3, "Thirty-Forty"},
-                { 4, 2, "Win for player1"},
-                { 2, 4, "Win for player2"},
-                { 4, 3, "Advantage player1"},
-                { 3, 4, "Advantage player2"},
-                { 5, 4, "Advantage player1"},
-                { 4, 5, "Advantage player2"},
-                { 15, 14, "Advantage player1"},
-                { 14, 15, "Advantage player2"},
-                { 6, 4, "Win for player1"},
-                { 4, 6, "Win for player2"},
-                { 16, 14, "Win for player1"},
-                { 14, 16, "Win for player2"},
+                { 0, 0, "Love-All" },                  // 0
+                { 1, 1, "Fifteen-All" },               // 1
+                { 2, 2, "Thirty-All"},                 // 2
+                { 3, 3, "Deuce"},                      // 3
+                { 4, 4, "Deuce"},                      // 4
+                { 5, 5, "Deuce"},                      // 5
+                { 6, 6, "Deuce"},                      // 6
+                { 7, 7, "Deuce"},                      // 7
+                { 1, 0, "Fifteen-Love"},               // 8
+                { 0, 1, "Love-Fifteen"},               // 9
+                { 2, 0, "Thirty-Love"},                // 10
+                { 0, 2, "Love-Thirty"},                // 11
+                { 3, 0, "Forty-Love"},                 // 12
+                { 0, 3, "Love-Forty"},                 // 13
+                { 4, 0, "Win for player1"},            // 14
+                { 0, 4, "Win for player2"},            // 15
+                { 2, 1, "Thirty-Fifteen"},             // 16
+                { 1, 2, "Fifteen-Thirty"},             // 17
+                { 3, 1, "Forty-Fifteen"},              // 18
+                { 1, 3, "Fifteen-Forty"},              // 19
+                { 4, 1, "Win for player1"},            // 20
+                { 1, 4, "Win for player2"},            // 21
+                { 3, 2, "Forty-Thirty"},               // 22
+                { 2, 3, "Thirty-Forty"},               // 23
+                { 4, 2, "Win for player1"},            // 24
+                { 2, 4, "Win for player2"},            // 25
+                { 4, 3, "Advantage player1"},          // 26
+                { 3, 4, "Advantage player2"},          // 27
+                { 5, 4, "Advantage player1"},          // 28
+                { 4, 5, "Advantage player2"},          // 29
+                { 15, 14, "Advantage player1"},        // 30
+                { 14, 15, "Advantage player2"},        // 31
+                { 6, 4, "Win for player1"},            // 32
+                { 4, 6, "Win for player2"},            // 33
+                { 16, 14, "Win for player1"},          // 34
+                { 14, 16, "Win for player2"},          // 35
         });
     }
 
