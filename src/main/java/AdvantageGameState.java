736a8c22fded8d7a87ceddb218eb0528a2d38d43
Thomas Eizinger <thomas@eizinger.io>
2017-11-22 14:34:24 +0100

    Make it green

--- src/main/java/AdvantageGameState.java
+++ src/main/java/AdvantageGameState.java
@@ -22,6 +22,6 @@ public class AdvantageGameState implements GameState {
 
     @Override
     public GameState playerTwoScored() {
-        return null;
+        return new WinGamestate(Player.PLAYER2);
     }
 }
